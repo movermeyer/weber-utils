2557e77633d24bb790734f06b463155eb3c37a3f
Rotem Yaari <vmalloc@gmail.com>
2014-04-05 21:25:57 +0300

    More tests

--- tests/test_strict_json.py
+++ tests/test_strict_json.py
@@ -14,14 +14,33 @@ def json_post(request):
     request.addfinalizer(lambda: returned.__exit__(None, None, None))
     return returned
 
-def test_strict_json():
+def test_nonexisting_key():
     with pytest.raises(BadRequest):
         strict_json["nonexisting"]
+
+def test_string_key_no_explicit_type():
     assert strict_json["string_key"] == "value"
 
-def test_strict_types():
+def test_invalid_type():
     with pytest.raises(BadRequest):
         strict_json["string_key", int]
-    assert strict_json["int_key", int] == 3
+
     with pytest.raises(BadRequest):
         strict_json["int_key", str]
+
+def test_type_checking_int():
+    assert strict_json["int_key", int] == 3
+
+def test_get_with_default():
+    assert strict_json.get("nonexisting", None) is None
+    assert strict_json.get("nonexisting", 3) == 3
+
+def test_get_with_default_no_type_checking():
+    assert strict_json.get(("nonexisting", int), "default") == "default"
+
+def test_get_with_default_type_checking():
+    assert strict_json.get(("int_key", int), "default") == 3
+    assert strict_json.get(("string_key", str), 31337) == "value"
+
+    with pytest.raises(BadRequest):
+        strict_json.get(("int_key", str), "default")
