c066d23e79c398eb73b2e39b1ccf18d749634356
Rotem Yaari <vmalloc@gmail.com>
2014-04-05 21:36:20 +0300

    Improve strict json

--- tests/test_strict_json.py
+++ tests/test_strict_json.py
@@ -1,11 +1,19 @@
 import json
+from contextlib import contextmanager
 
 from flask import Flask
 from werkzeug.exceptions import BadRequest
 
 import pytest
 from weber_utils import strict_json
+from weber_utils.request_utils import HTTPException
 
+
+@contextmanager
+def _assert_bad_request_context():
+    with pytest.raises(HTTPException) as caught:
+        yield caught
+    assert caught.value.code == 400
 
 @pytest.fixture(autouse=True, scope="module")
 def json_post(request):
@@ -15,17 +23,17 @@ def json_post(request):
     return returned
 
 def test_nonexisting_key():
-    with pytest.raises(BadRequest):
+    with _assert_bad_request_context():
         strict_json["nonexisting"]
 
 def test_string_key_no_explicit_type():
     assert strict_json["string_key"] == "value"
 
 def test_invalid_type():
-    with pytest.raises(BadRequest):
+    with _assert_bad_request_context():
         strict_json["string_key", int]
 
-    with pytest.raises(BadRequest):
+    with _assert_bad_request_context():
         strict_json["int_key", str]
 
 def test_type_checking_int():
@@ -42,5 +50,5 @@ def test_get_with_default_type_checking():
     assert strict_json.get(("int_key", int), "default") == 3
     assert strict_json.get(("string_key", str), 31337) == "value"
 
-    with pytest.raises(BadRequest):
+    with _assert_bad_request_context():
         strict_json.get(("int_key", str), "default")
--- weber_utils/strict.py
+++ weber_utils/strict.py
@@ -3,6 +3,7 @@ from flask import abort, request
 from sentinels import NOTHING
 
 from ._compat import PY2
+from .request_utils import error_abort
 
 _logger = logbook.Logger(__name__)
 
@@ -20,8 +21,11 @@ class _StrictJSON(object):
             return default
         if field_type is str and PY2:
             field_type = (str, unicode)
+        elif not isinstance(field_type, tuple):
+            field_type = (field_type,)
+
         if (field_type is bool and not isinstance(returned, bool)) or not isinstance(returned, field_type):
-            _logger.debug("Field {0!r} should be {1.__name__}, but is {2!r}", field_name, field_type, type(returned))
+            _logger.debug("Field {0!r} should be {1.__name__}, but is {2!r}", field_name, field_type[0], type(returned))
             _abort_bad_request()
         return returned
 
@@ -35,4 +39,4 @@ class _StrictJSON(object):
 strict_json = _StrictJSON()
 
 def _abort_bad_request():
-    abort(400)
+    error_abort(400, "bad request")
